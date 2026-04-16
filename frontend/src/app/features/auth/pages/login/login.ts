4b86fabd88608d2a2ef1e847794b46925c2352d4
Davo <davo@email.com>
2026-04-16 08:56:43 -0400

    Feature Login

--- frontend/src/app/features/auth/pages/login/login.ts
+++ frontend/src/app/features/auth/pages/login/login.ts
@@ -1,11 +1,51 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  standalone: true,
+  
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html'
 })
-export class Login {
+export class LoginComponent {
+  form: FormGroup;
+  error: string | null = null;
+  loading = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  entrar() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    this.error = null;
+
+    const { email, password } = this.form.value;
+
+    
+    console.log('Tentando acesso com:', email);
+
+    if (email === 'admin@epipim.com.br' && password === '123456') {
+      
+      this.router.navigate(['/dashboard']);
+    } else {
+      
+      this.error = 'E-mail ou senha incorretos. Verifique suas credenciais.';
+      this.loading = false;
+    }
+  }
 }
